dd01ebcc34c6de9b8c3d54c40ea63e3636d7574a
Rushi282 <rushimajale5453@gmail.com>
2024-08-31 10:07:47 +0530

    This is Pattern Code

---
+++ Pattern.java
@@ -0,0 +1,30 @@
+class Pattern{
+	public static void main(String[] args){
+		int row = 5;
+		int star = 1;
+		int space = row-1;
+		for(int i=0; i<row; i++){
+			for(int k=0; k<space; k++){
+				System.out.print(" ");
+
+			}
+			for(int j=0; j<star; j++){
+				System.out.print("*");
+			}
+		System.out.println();
+		star++;  //5
+		space--; //0
+		}
+		for(int i=0; i<row; i++){
+			for(int k=0; k<space; k++){
+				System.out.print(" ");
+			}
+			for(int j=0; j<star; j++){
+				System.out.print("*");
+			}
+			System.out.println();
+			star--;
+			space++;
+		}		
+	}
+}
