95917dc3d544c5f6b8c954d7965745793d04971b
Rushi282 <rushimajale5453@gmail.com>
2024-08-31 10:09:41 +0530

    Comment added in code

--- Pattern.java
+++ Pattern.java
@@ -3,18 +3,20 @@ class Pattern{
 		int row = 5;
 		int star = 1;
 		int space = row-1;
-		for(int i=0; i<row; i++){
-			for(int k=0; k<space; k++){
+		for (int i = 0; i < row; i++) {
+			for (int k = 0; k < space; k++) {
 				System.out.print(" ");
 
 			}
-			for(int j=0; j<star; j++){
+			for (int j = 0; j < star; j++) {
 				System.out.print("*");
 			}
-		System.out.println();
-		star++;  //5
-		space--; //0
+			System.out.println();
+			star++; //5
+			space--; //0
 		}
+		
+		//Reverse the pattern
 		for(int i=0; i<row; i++){
 			for(int k=0; k<space; k++){
 				System.out.print(" ");
